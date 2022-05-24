4cc5aa83eb2cba66b1f8070be772f2b296e59bfb
Adorn Choga <adornchoga@gmail.com>
2022-05-24 13:02:33 +0200

    Write specs for categories controller

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
+    before(:each) do
       get '/categories/index'
+    end
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+    it 'renders the index page' do
+      expect(response).to render_template(:index)
+    end
+    it 'index page has Categores text' do
+      expect(response.body).to include 'Categories'
     end
   end
 end
