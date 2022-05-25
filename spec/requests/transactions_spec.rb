f50a200208ce4442998e1e47ee9362a2a4bd2c89
Adorn Choga <adornchoga@gmail.com>
2022-05-25 09:46:34 +0200

    Add specs for new transaction page

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Transactions', type: :request do
+  before(:each) do
+    @user = create(:user)
+    @category = create(:category, user_id: @user.id)
+  end
   describe 'GET /index' do
-    before(:each) do
-      @user = create(:user)
-      @category = create(:category, user_id: @user.id)
-      get category_transactions_path(@category.id)
-    end
+    before(:example) { get category_transactions_path(@category.id) }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
@@ -19,12 +19,19 @@ RSpec.describe 'Transactions', type: :request do
     end
   end
 
-  # describe "GET /new" do
-  #   it "returns http success" do
-  #     get "/transactions/new"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /new" do
+    before(:example) { get new_category_transaction_path(@category.id) }
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the new category page' do
+      expect(response).to render_template(:new)
+    end
+    it 'have New Transaction text in the body of the page' do
+      expect(response.body).to include 'New Transaction'
+    end
+  end
 
   # describe "GET /create" do
   #   it "returns http success" do
