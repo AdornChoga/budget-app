dba5b011dcca1274184e292cac3e099a4db00d89
Adorn Choga <adornchoga@gmail.com>
2022-05-26 11:18:12 +0200

    Install and setup active storage for user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
 
   has_many :categories, dependent: :destroy
   has_many :financial_transactions, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
   validates :name, presence: true
 end
