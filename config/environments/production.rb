26ac8a963f03547ec189de5e9cb790c85af509a0
Adorn Choga <adornchoga@gmail.com>
2022-05-26 17:50:09 +0200

    Setup letter opener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,13 +93,18 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://budget-pal-rails.herokuapp.com/' }
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => "sanchyclips@gmail.com",
-    :password             => "3bY4hR^QE#p^wh3z",
-    :authentication       => :plain,
-    :enable_starttls_auto => true
-  }
+  # Letter Opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+ 
 end
+
+# config.action_mailer.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "gmail.com",
+#   :user_name            => "sanchyclips@gmail.com",
+#   :password             => "3bY4hR^QE#p^wh3z",
+#   :authentication       => :plain,
+#   :enable_starttls_auto => true
+# }
