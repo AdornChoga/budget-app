2458c99e490aae93416944a303b9ca9aea08a52b
Adorn Choga <adornchoga@gmail.com>
2022-05-26 17:36:08 +0200

    Edit default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://budget-pal-rails.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://budget-pal-rails.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
